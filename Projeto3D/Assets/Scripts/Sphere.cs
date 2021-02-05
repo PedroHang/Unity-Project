a03f5a431064653d1ec61dc74e02a80786bad9eb
Pedro Lucas Azevedo Hang <46850459+PedroHang@users.noreply.github.com>
2021-02-05 12:40:21 -0300

    Update 05/02

---
+++ Projeto3D/Assets/Scripts/Sphere.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine.Audio;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sphere : MonoBehaviour
+{
+    public bool onGround;
+    public float speed = 6f;
+    private Rigidbody rb;
+
+    private bool isInArea = false;
+
+    void Start()
+    {
+        onGround = true;
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    void Update()
+    {
+        transform.position += Vector3.forward * speed * Time.deltaTime;  // MOVING
+
+        
+        if(Input.GetKey(KeyCode.A)){
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.D)){
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        if(onGround){
+            if(Input.GetButtonDown("Jump")){
+                rb.velocity = new Vector3(0f, 7f, 0f);
+                onGround = false;
+            }
+        }
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    void OnCollisionEnter(Collision other) {
+        if(other.gameObject.CompareTag("Ground")){
+            onGround = true;
+        }
+
+        if(other.collider.tag == "Enemy"){
+            Replay();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        isInArea = true;
+        if(other.gameObject.CompareTag("Coins")){
+            Destroy(other.gameObject);
+            FindObjectOfType<AudioManager>().Play("Coin");
+        }
+    }
+    
+}
---
+++ Runner Prototype/Assets/SCRIPTS/Drag.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Drag : MonoBehaviour
+{
+    private float startPosX;
+    private float startPosY;
+    private bool isBeingHeld = false;
+    private bool Dropped;
+    public float speed = 4.5f;
+    
+
+    void Start()
+    {
+        speed = -4.5f;
+    }
+
+    void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+
+        if (isBeingHeld == true)
+        {
+
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x, mousePos.y, 0);
+        }
+
+         if (Dropped == true)
+        {
+            speed = 0f;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            isBeingHeld = true;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        isBeingHeld = false;
+        Dropped = true;
+    }
+}
