634c6b67a6bfbbf216de067640a2d43814584ead
Pedro Lucas Azevedo Hang <46850459+PedroHang@users.noreply.github.com>
2021-02-26 14:59:25 -0300

    Update 26/02

---
+++ Projeto3D/Assets/Scripts/Player.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine.Audio;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public bool onGround;
+    public float speed = 6f;
+    private Rigidbody rb;
+    private const int MAX_JUMP = 2;
+    private int currentJump = 0;
+
+    private bool isInArea = false;
+ 
+    Animator animator;
+    void Start()
+    {
+        onGround = true;
+        rb = GetComponent<Rigidbody>();
+
+        animator = GetComponent<Animator>();
+        
+    }
+
+    void Update()
+    {
+        transform.position += Vector3.forward * speed * Time.deltaTime;  // MOVING
+
+        
+        if(Input.GetKey(KeyCode.A)){
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.D)){
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.W)){
+            speed = 10f;
+        }
+
+        if(onGround || MAX_JUMP > currentJump){
+            if(Input.GetButtonDown("Jump")){
+                rb.velocity = new Vector3(0f, 6f, 2f);
+                animator.SetBool("isJumping", true);
+                currentJump++;
+                onGround = false;
+            }
+        }
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    void OnCollisionEnter(Collision other) {
+        if(other.gameObject.CompareTag("Ground")){
+            speed = 6f;
+            onGround = true;
+            currentJump = 0;
+            animator.SetBool("isJumping", false);
+        }
+
+        if(other.gameObject.CompareTag("Mud")){
+            speed = 3f;
+            onGround = true;
+            animator.SetBool("isJumping", false);
+            animator.speed = 0.8f;
+        }
+
+        if(other.collider.tag == "Enemy"){
+            Replay();
+        }
+
+        if(other.collider.tag == "End"){
+            Replay();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        isInArea = true;
+        if(other.gameObject.CompareTag("Coins")){
+            Destroy(other.gameObject);
+            FindObjectOfType<AudioManager>().Play("Coin");
+        }
+    }
+    
+}
---
+++ Projeto3D/Assets/Scripts/TP.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine.Audio;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TP : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision other) {
+        if(other.gameObject.CompareTag("Player")){
+            FindObjectOfType<AudioManager>().Play("Tp");
+            other.gameObject.transform.position = new Vector3(220, -2, 466);
+        }
+    }
+}
